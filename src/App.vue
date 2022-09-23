28692f02f8186dd84b6b712a519b499c40d22d98
mresire <user@Users-MacBook-Air.local>
2022-09-23 08:39:23 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="app">
+    <p>{{ jobs[0].location}}</p>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent, reactive, ref, toRefs} from 'vue';
+import Job from './types/Job'
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
+  components: {},
+  setup() {
+    // const state = reactive({
+    //   name: 'Link',
+    //   age: 29 as string | number
+    // })
+
+    // state.name = 123 // cannot change type
+    // return {
+    //   ...toRefs(state)
+    // }
+
+    // const name = ref('Link')
+    // const age = ref<number | string >(29) 
+
+    // return { name, age}
+
+    const jobs = ref<Job[]>([
+      { title: 'DevOps', location: 'Silicon valley', salary: 150000, id:'1'},
+      { title: 'quarryman', location: 'death mountain', salary: 40000, id: '2' },
+      { title: 'flute player', location: 'the lost woods', salary: 35000, id: '3' },
+      { title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4' },
+      { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5' }
+    ])
+
+    return { jobs}
+
   }
+  // data () {
+  //   return {
+  //     name: 'Link',
+  //     age: 29 as number | string
+  //   }
+  // },
+  // methods: {
+  //   changeName(name: string) {
+  //     this.name = name;
+  //   },
+  //   changeAge(age: number | string ) {
+  //     this.age = age;
+  //   }
+  // }
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
